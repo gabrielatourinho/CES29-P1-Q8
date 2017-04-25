2a489cf569a26df797825f98289df521f4e3a7a6
Gabriela Tourinho <gabrielantourinho@gmail.com>
2017-04-25 08:35:04 -0300

    Commit inicial: projeto maven criado

---
+++ src/main/java/P1Gabriela/Q8/App.java
@@ -0,0 +1,13 @@
+package P1Gabriela.Q8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
